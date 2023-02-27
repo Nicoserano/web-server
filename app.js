5c3b748e9d03f5285ddbab18c2fac510a846de65
Ronald serrano <ronalserrano831@gmail.com>
2023-02-26 22:16:45 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const hbs= require('hbs');
+require('dotenv').config();
+
+const app= express();
+const port =process.env.PORT;
+
+
+//handlebars
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+
+
+app.use(express.static('public'));
+app.get('/index', (req,res)=>{
+    res.render('home',{
+        nombre:' Ronald Serrano',
+        titulo: 'Curso Node'
+    })
+});
+app.get('/generic', (req,res)=>{
+    res.render('generic',{
+        nombre:' Ronald Serrano',
+        titulo: 'Curso Node'
+    })
+});
+app.get('/elements', (req,res)=>{
+    res.render('elements',{
+        nombre:' Ronald Serrano',
+        titulo: 'Curso Node'
+    })
+});
+app.listen(port);
